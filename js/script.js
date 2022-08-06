0448c326d065515540b56001881792310b50bfd0
Anna Vasilevich <dzichonka@gmail.com>
2022-08-06 12:59:33 +0200

    PULSE

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+// // $(document).ready(function () {
+// //     $('.carousel__wrapper').slick({
+// //         speed: 1000,
+// //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.svg"></button>',
+// //         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
+// //         responsive: [
+// //             {
+// //                 breakpoint: 768,
+// //                 settings: {
+// //                     arrows: false,
+// //                     dots: true,
+// //                 }
+// //             }
+// //         ],
+// //     });
+// // });
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+// var slider = tns({
+//     container: '.carousel__wrapper',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: true,
+//     controls: false,
+//     autoplayButtonOutput: false,
+//     navPosition: "bottom",
+//     responsive: {
+//         768: {
+//             controls: false,
+//         },
+//     }
+//     // nav: false,
+//     // controlsText: [
+//     //     '<img src="icons/arrows/left.svg">',
+//     //     '<img src="icons/arrows/right.svg">',
+//     // ]
+// });
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// });
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// });
